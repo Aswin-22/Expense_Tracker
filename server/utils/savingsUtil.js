16173b09373f3ff03b7d2814f64305ffc95172e7
Aswin-22 <Aswin.luck543@gmail.com>
2025-08-02 17:42:06 +0530

    Fixed Transaction abort issue

--- server/utils/savingsUtil.js
+++ server/utils/savingsUtil.js
@@ -1,16 +1,23 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-export async function withTransaction(fn) {
+async function withTransaction(fn) {
   const session = await mongoose.startSession();
+  let transactionCompleted = false;
+  
   try {
     session.startTransaction();
     const result = await fn(session);
     await session.commitTransaction();
+    transactionCompleted = true;
     return result;
   } catch (err) {
-    await session.abortTransaction();
+    if (!transactionCompleted && session.inTransaction()) {
+      await session.abortTransaction();
+    }
     throw err;
   } finally {
     session.endSession();
   }
 }
+
+module.exports = { withTransaction };
